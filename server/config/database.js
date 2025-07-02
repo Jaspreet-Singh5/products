664c31774385198d1cd11968b31ef1cf8b61ebdc
Jaspreet-Singh5 <jaswindersingh705@gmail.com>
2025-07-02 21:07:08 +0530

    refactor: update database connection to use ES6 modules and dotenv for environment variables

--- server/config/database.js
+++ server/config/database.js
@@ -1,12 +1,14 @@
-const mongoose = require("mongoose");
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
 const MONGO_URI = process.env.MONGO_URI;
 
 const connectDatabase = () => {
-  mongoose
-    .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-      console.log("Mongoose Connected");
+    mongoose.connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
+        console.log('Mongoose Connected');
     });
 };
 
-module.exports = connectDatabase;
+export default connectDatabase;
